840eaa299d6a9ba4f5f0f914addcfdcf6a71babe
kkgu <kkgu@localhost>
2025-01-22 16:08:01 -0600

    test

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -30,10 +30,20 @@ public class RobotInstructions  {
 		 * as you desire.
 		 */
 		robot.forward(60);
-		robot.turnLeft(90);
+		robot.turnLeft(60);
 		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.turnRight(60);
+		robot.forward(60);
+		robot.forward(60);
+		robot.turnLeft(60);
+		robot.forward(23);
+		robot.turnRight(60);
+		robot.forward(60);
+		robot.forward(60);
+		robot.turnLeft(60);
+		robot.forward(23);
+		robot.turnRight(60);
+		robot.forward(60);
 	}
 
 }
